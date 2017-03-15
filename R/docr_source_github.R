e8fa70af76d0ba2d5ab501ee0b9b04272a849fec
Carl Boneri <carl.boneri@fas-advisory.com>
2017-03-15 10:44:02 -0400

    Added sourcing options
    New argument added to allow the library to be sourced into the local environment.

--- R/docr_source_github.R
+++ R/docr_source_github.R
@@ -4,6 +4,11 @@
 #'
 #' @param output_dir The directory to store the files to.
 #'
+#' @param source_on_load logical After the files have been moved into the local 
+#'  directory, should they be sourced into the global environment. Defaults to
+#'  true.
+#' 
+#' 
 #' @family Documentation functions
 #'
 #' @author Carl S.A. Boneri, \email{carl@@gmail.com}
@@ -18,16 +23,20 @@
 #' @export
 
 
-docr.install_r_only <- function(output_dir = "R"){
+docr.install_r_only <- function(output_dir = "R", source_on_load = TRUE){
+  
   if(!dir.exists(output_dir)){
     dir.create(output_dir)
   }
-
+  
   o <- suppressMessages(suppressWarnings(
-  git.recursive("CarlBoneri/roxydoc2", filter_pat = "R$") %>% .$raw %>% lapply(., function(i){
-    out_path <- sprintf("%s/%s",output_dir,basename(i))
-    download.file(i, out_path, quiet = TRUE)
-  })
+    git.recursive("CarlBoneri/roxydoc2", filter_pat = "R$") %>% .$raw %>% 
+      lapply(., function(i){
+        out_path <- sprintf("%s/%s",output_dir,basename(i))
+        download.file(i, out_path, quiet = TRUE)
+        if(source_on_load && file.exists(out_path)){
+          base::source(out_path, echo = FALSE, print.eval = FALSE)
+        }
+      })
   ))
-
 }
